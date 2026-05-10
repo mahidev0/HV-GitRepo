f32d687f1bedde1d7929438008b80455660c8aeb
Devops-Worlds <95467021+mahidev0@users.noreply.github.com>
2026-05-10 14:26:24 +0530

    Added add function in app.py

--- Git_assignment/Question1/app.py
+++ Git_assignment/Question1/app.py
@@ -1,3 +1,10 @@
 # this is Github Assignment via HeroVied Question1
 
-print ("Hello HeroVied, Welcome to Github Assignment")
+print ("Hello HeroVied, Welcome to Github Assignment question1")
+
+print("Welcome to Git Assignment Question2")
+
+def add(a, b):
+    return a + b
+
+print(add(10, 20))
